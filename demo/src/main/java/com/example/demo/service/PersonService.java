5cd1d210ca152f67305689e50d2a7d9273fc3c70
AshrafDesai <asharafrazadesai2002@gmail.com>
2024-06-10 13:04:23 +0530

    Spring Boot Basics

---
+++ demo/src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,31 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class PersonService {
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(UUID.randomUUID(), person);
+    }
+
+    public  List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+}
